4b2054cddea10ca215b43d1cc03fe35268ad2515
rameessiyad <rameesmsiyadofficial786@gmail.com>
2025-02-12 16:35:41 +0530

    admin functionalities added

---
+++ controllers/admin-controller.js
@@ -0,0 +1,44 @@
+const User = require("../models/user-model");
+const Ads = require("../models/bike-model");
+
+module.exports = {
+  //get analytics
+  //GET /api/v1/admin/analytics
+  //@access private admin
+  getAnalytics: async (req, res, next) => {
+    try {
+      const totalUsers = await User.countDocuments();
+      const totalAds = await Ads.countDocuments();
+      const activeAds = await Ads.countDocuments({ status: "active" });
+      const inactiveAds = await Ads.countDocuments({ status: "inactive" });
+
+      res.status(201).json({
+        success: true,
+        data: {
+          totalUsers,
+          totalAds,
+          activeAds,
+          inactiveAds,
+        },
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  //get reported ads
+  //GET /api/v1/admin/reported-ads
+  //@access private admin
+  getReportedAds: async (req, res, next) => {
+    try {
+      const reportedAds = await Ads.find({ status: "reported" });
+
+      res.status(201).json({
+        success: true,
+        data: reportedAds,
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
+};
--- routes/admin-route.js
+++ routes/admin-route.js
@@ -1,3 +1,12 @@
-const router = require('express').Router();
+const {
+  getAnalytics,
+  getReportedAds,
+} = require("../controllers/admin-controller");
+const { authMiddleware, adminOnly } = require("../middleware/authMiddleware");
+
+const router = require("express").Router();
+
+router.get("/analytics", authMiddleware, adminOnly, getAnalytics);
+router.get("/reported-ads", authMiddleware, adminOnly, getReportedAds);
 
 module.exports = router;
